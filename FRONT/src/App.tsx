4e41cad43c8dfdebb191e1430f8f8dd5d28b9d1d
Nube111 <javalle191@gmail.com>
2024-11-23 20:00:40 -0500

    avance con front

---
+++ FRONT/src/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+
+
+import React from 'react'; 
+
+function App(): JSX.Element {
+  return (
+    <h1>todo mvc</h1>
+  );
+}
+
+export default App; 
